4c97c9897d23b12e5479f60d024b9be93c07b30f
Adam-Harte <adamharte11@hotmail.co.uk>
2021-08-11 21:06:18 +0100

    image upload added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const fs = require('fs');
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -50,6 +51,25 @@ app.use((req, res, next) => {
   next();
 });
 
+app.use('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+
+  if (!req.file) {
+    return res.status(200).json({ message: 'No file provided!' });
+  }
+
+  if (req.body.oldPath) {
+    clearImage(req.body.oldPath);
+  }
+
+  return res.status(201).json({
+    message: 'File stored.',
+    filePath: req.file.path
+  });
+});
+
 app.use(isAuth);
 
 app.use('/graphql', grahpqlHttp({
@@ -89,3 +109,8 @@ mongoose
     app.listen(8080);
   })
   .catch(err => console.log(err));
+
+  const clearImage = filePath => {
+    filePath = path.join(__dirname, '..', filePath);
+    fs.unlink(filePath, err => console.log(err));
+  };
